293f8533e703330ce6fa0671418469ee08955edd
DarlanJurak <darlan.jurak@gmail.com>
2019-12-27 23:24:45 -0300

    added differents waypoints and initial positions for headon, crossing from left and crossing for right; decreased diff2 speed; increased laser ray

--- usv_base_ctrl/scripts/diff_control_heading.py
+++ usv_base_ctrl/scripts/diff_control_heading.py
@@ -51,8 +51,8 @@ def thruster_ctrl_msg():
         left = 0
     if (right < 0):
         right  = 0  
-    left    *=  0.0025
-    right   *=  0.0025
+    left    *=  0.0015
+    right   *=  0.0015
 
 
     msg.position = [left, right]
